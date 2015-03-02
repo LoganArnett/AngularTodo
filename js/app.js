033d16cce0d4de8dc2e1e691ef2e523b7f0cfd0d
LoganArnett <Loganarn5@gmail.com>
2015-03-02 17:33:41 -0500

    todomvc angular

--- js/app.js
+++ js/app.js
@@ -1,6 +1,115 @@
-(function (window) {
-	'use strict';
+'use strict'
 
-	// Your starting point. Enjoy the ride!
+angular.module('todoAngular', [])
 
-})(window);
+.controller('MainCtrl', ['filterFilter', function(filterFilter){
+    var self = this;
+
+// Empty input
+    this.newTodo = '';
+// Empty array to push into
+    this.todoList = [];
+// Length of Array todo for item count intialize
+    this.itemsLeft =  self.todoList.length
+    this.completeFilter = filterFilter(this.todoList, {completed:true}).length
+// Default to no editing mode
+    this.todoEdit = false;
+
+// Add new todo on submit
+    this.addTodo = function(){
+        
+        var newTask = this.newTodo.trim();
+        var list = this.todoList;
+        
+        if(newTask.length === 0){
+            console.log("boo")
+            return;
+        }
+        // Push new objects into the array with default values
+            list.push({
+                "label": newTask,
+                "input": newTask,
+                "todoEdit": false,
+                "completed": false,
+                "active": true
+            });
+        // Return new todo input to empty and increment counter
+            this.itemsLeft = this.todoList.length
+            return this.newTodo = '';   
+        }
+    
+// Adds completed == true to object in array
+    this.done = function(todos){
+        todos.active = false;
+        return todos.completed = true;
+    }
+    
+// Add editing class on DoubleClick
+    this.editTodo = function(todo){
+       return todo.todoEdit = true;
+    }
+    
+// Submitting edits
+    this.editing = function(todos){
+        var list = this.todoList;
+        
+        // Checks to see if todo was erased
+        if(todos.label.length == 0){
+            list.splice(list.indexOf(todos), 1)
+        }
+        todos.todoEdit = false;
+    }
+    
+// Delete individual todos
+    this.deleteTodo = function(todos){
+        this.todoList.splice(this.todoList.indexOf(todos), 1);
+        self.items = self.todoList.length;
+    }
+    
+// Add completed for strikethrough
+    this.completed = function(todo){
+       return todo = true;
+          
+    }
+        
+// Deletes all todos marked completed 
+    this.clearCompleted = function(){
+        var list = this.todoList;
+        
+        this.todoList = list.filter(function(todo){
+            
+            return todo.completed == false;
+        });
+        self.items = self.todoList.length;
+    }
+    
+// Show the clear completed option when a todo is checked off
+    this.noComplete = function(){
+        var list = this.todoList;
+        
+        if(this.todoList.some(this.completed)){
+            return true;
+        }
+        return false;
+    }
+    
+// Check off every todo at once
+    this.allCompleted = function(allDone){
+        var list = this.todoList
+        if(allDone == true){
+            list.forEach(function(todo){
+                self.itemsLeft = 0;
+                return todo.completed = true;
+            })
+            
+        }
+        else {
+            list.forEach(function(todo){
+                self.itemsLeft = list.length
+                return todo.completed = false;
+            })
+        }
+    }
+}])
+
+
